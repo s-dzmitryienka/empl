2b2a2587387cb74b2af7a1864a20a7bb3f9ceb03
s.dzmitryienka <s.dzmitryienka@exaft.com>
2022-07-31 13:33:49 +0300

    Add docker-compose

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.10.5-buster
+
+RUN apt-get update \
+    && DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends install binutils libproj-dev apt-transport-https ca-certificates
+
+ENV PYTHONUNBUFFERED 1
+RUN pip3 install pipenv
+
+COPY Pipfile .
+COPY Pipfile.lock .
+
+RUN pipenv lock --dev -r > requirements.txt
+RUN pip install -r requirements.txt
+COPY . /app
+WORKDIR /app
--- src/core/config.py
+++ src/core/config.py
@@ -4,14 +4,22 @@ from starlette.config import Config
 
 config = Config('.env')
 
-DATABASE_URL = config('EE_DATA_BASE_URL', cast=str, default='')
+# DATABASE_URL = config('EE_DATA_BASE_URL', cast=str, default='')
+DB = 'postgresql'
+DB_HOST = os.getenv('DB_HOST')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
+DB_PORT = os.getenv('DB_PORT')
 
+DATABASE_URL = f"{DB}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-# Celery config
-CELERY_RABBIT_HOST = os.getenv('CELERY_RABBIT_HOST', 'localhost')
-CELERY_RABBIT_PORT = os.getenv('CELERY_RABBIT_PORT', '5672')
-CELERY_RABBIT_USER = os.getenv('CELERY_RABBIT_USER', 'guest')
-CELERY_RABBIT_PASSWORD = os.getenv('CELERY_RABBIT_PASSWORD', 'guest')
-CELERY_MESSAGE_BROKER = os.getenv('CELERY_MESSAGE_BROKER', 'amqp')
+# msg broker conf
+RABBIT_HOST = os.getenv('RABBIT_HOST', 'localhost')
+RABBIT_PORT = os.getenv('RABBIT_PORT', '5672')
+RABBIT_USER = os.getenv('RABBIT_USER', 'guest')
+RABBIT_PASSWORD = os.getenv('RABBIT_PASSWORD', 'guest')
 
-CELERY_BROKER_URL = f'{CELERY_MESSAGE_BROKER}://{CELERY_RABBIT_USER}:{CELERY_RABBIT_PASSWORD}@{CELERY_RABBIT_HOST}:{CELERY_RABBIT_PORT}'
+# celery conf
+MESSAGE_BROKER = os.getenv('MESSAGE_BROKER', 'amqp')
+CELERY_BROKER_URL = f'{MESSAGE_BROKER}://{RABBIT_USER}:{RABBIT_PASSWORD}@{RABBIT_HOST}:{RABBIT_PORT}'
